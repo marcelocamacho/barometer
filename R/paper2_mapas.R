81d1f01aecf9f11a880dab4f299e7af3f48129b4
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2022-05-25 07:33:52 -0300

    Geração dos mapas para a mesoregião nordeste e sudeste

--- R/paper2_mapas.R
+++ R/paper2_mapas.R
@@ -18,45 +18,81 @@
 
   mapaUF = readShapePoly("./inst/shapefile_pa_municipios/2000/municipios/15MU500G.shp")
   mapaUF = mapaUF[mapaUF@data$CODIGO %in% df_nordeste$codigo,]
+  mapaUF = mapaUF[mapaUF@data$SEDE == TRUE,]
   mapaData = attr(mapaUF, 'data')
   mapaData$Index = row.names(mapaData)
 
   #Calcular centroids
   mapaUF_df<-fortify(mapaUF)
-  mapaUF_df<-unique(mapaUF_df)
   mapaUF_df$Index<-as.factor(mapaUF_df$id)
+
   centroids <- setNames(do.call("rbind.data.frame", by(mapaUF_df, mapaUF_df$Index, function(x) {
    Polygon(x[c('long', 'lat')])@labpt
   })), c('long', 'lat'))
-  centroids<-unique(centroids)
-  centroids$factors <- levels(mapaUF_df$Index)
-
+  centroids$Index <- levels(mapaUF_df$Index)
+  centroids$factors <- 1:nrow(centroids)
   rm(mapaUF_df)
-
-  trueCentroids = rgeos::gCentroid(mapaUF,byid=TRUE)
-  trueCentroids = fortify(trueCentroids)
-  trueCentroids$id = row.names(trueCentroids)
-
-
-  plot(mapaUF)
-  text(trueCentroids,cex = 0.7)
-  points(coordinates(mapaUF),pch=1)
-  points(trueCentroids,pch=2)
-
-
   df_nordeste = merge(df_nordeste, coresDasCategorias)
   mapaData_nordeste=merge(mapaData, df_nordeste,by.x="CODIGO",by.y="codigo")
   attr(mapaUF, 'data')= df_nordeste
 
   # Configurando tela (reduzindo as margens da figura)
   parDefault = par(no.readonly = T)
-  layout(matrix(c(1,2),nrow=2),widths= c(1,1), heights=c(4,1))
+  layout(matrix(c(1,2),nrow=2),widths= c(1,1), heights=c(5,1))
   par (mar=c(0,0,0,0))
   plot(mapaUF, col=as.character(mapaData_nordeste$Cores))
-  text(centroids$long,centroids$lat,centroids$factors,cex = 0.7,pos = 4,col = "black", offset=-0.2)
+  text(centroids$long,centroids$lat,centroids$factors,cex = 0.6,col = "black",pos = 1,offset = .15)
+  #text(mapaUF@data$LONGITUDES,mapaUF@data$LATITUDESE,row.names(mapaUF@data),cex = 0.6,col = "black",pos = 1,offset = 0.2)
   plot(1,1,pch=NA, axes=F)
+  par (mar=c(0,0,0,0))
   legend(x='center', legend=rev(coresDasCategorias$nivel),
          box.lty=0, fill=rev(paletaDeCores),cex=.8, ncol=2,
          title='Classificação da sustentabilidade na região nordeste Paraense')
-}
+ #mapaData[mapaData$Index == centroids[centroids$factors==21,]$Index,]
+ rm(df_nordeste,mapaData_nordeste,mapaUF_df,parDefault,trueCentroids,centroids,mapaUF,munData,mapaData)
+ }
+
+ # Mesoregião Sudeste
+ {
+  df_sudeste<-df %>%
+   select(-valor,-INDICADOR,-DIMENSAO) %>%
+   filter(nome_mesoreg =="Sudeste Paraense") %>%
+   group_by(nome_mesoreg,nome_microreg,codigo,municipio) %>%
+   summarise(bs=mean(bs))
+  df_sudeste$nivel<-as.factor(nivelBS(df_sudeste$bs))
+
+  mapaUF = readShapePoly("./inst/shapefile_pa_municipios/2000/municipios/15MU500G.shp")
+  mapaUF = mapaUF[mapaUF@data$CODIGO %in% df_sudeste$codigo,]
+  mapaUF = mapaUF[mapaUF@data$SEDE == TRUE,]
+  mapaData = attr(mapaUF, 'data')
+  mapaData$Index = row.names(mapaData)
+
+  #Calcular centroids
+  mapaUF_df<-fortify(mapaUF)
+  mapaUF_df$Index<-as.factor(mapaUF_df$id)
+
+  centroids <- setNames(do.call("rbind.data.frame", by(mapaUF_df, mapaUF_df$Index, function(x) {
+   Polygon(x[c('long', 'lat')])@labpt
+  })), c('long', 'lat'))
+  centroids$Index <- levels(mapaUF_df$Index)
+  centroids$factors <- 1:nrow(centroids)
+  rm(mapaUF_df)
+  df_sudeste = merge(df_sudeste, coresDasCategorias)
+  mapaData_sudeste=merge(mapaData, df_sudeste,by.x="CODIGO",by.y="codigo")
+  attr(mapaUF, 'data')= df_sudeste
+
+  # Configurando tela (reduzindo as margens da figura)
+  parDefault = par(no.readonly = T)
+  layout(matrix(c(1,2),nrow=2),widths= c(1,1), heights=c(5,1))
+  par (mar=c(0,0,0,0))
+  plot(mapaUF, col=as.character(mapaData_sudeste$Cores))
+  text(centroids$long,centroids$lat,centroids$factors,cex = 0.5,col = "black")
+  #text(mapaUF@data$LONGITUDES,mapaUF@data$LATITUDESE,row.names(mapaUF@data),cex = 0.6,col = "black",pos = 1,offset = 0.2)
+  plot(1,1,pch=NA, axes=F)
+  par (mar=c(0,0,0,0))
+  legend(x='center', legend=rev(coresDasCategorias$nivel),
+         box.lty=0, fill=rev(paletaDeCores),cex=.8, ncol=2,
+         title='Classificação da sustentabilidade na região sudeste Paraense')
+ #mapaData[mapaData$Index == centroids[centroids$factors==21,]$Index,]
+ }
 
